b74dbaef580694e84aa4097118a21aa09aab8890
DevMinthiha <minthiha2681996@gmail.com>
2023-05-23 18:40:46 +0630

    update

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -1,10 +1,10 @@
 export const paths = {
-  home: "/dashboard",
+  home: "/",
   create_product: "/product/create",
   product_list: "/product/list",
   create_category: "/category/create",
   category_list: "/category/list",
   customers: "/customers",
-  login: "/",
+  login: "/login",
   register: "/register",
 };
